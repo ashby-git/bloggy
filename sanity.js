28db40ab209fda98200ebf5db03c48125a8f07f1
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-07-21 17:37:20 +0900

    📦️ add sanity image-url

--- sanity.js
+++ sanity.js
@@ -1,8 +1,5 @@
-import {
-  createClient,
-  createImageUrlBuilder,
-  createCurrentUserHook,
-} from "next-sanity";
+import { createClient, createCurrentUserHook } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   /**
