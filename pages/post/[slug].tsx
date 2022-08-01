47d04ecd1975ba67e63c847701afc0f8bc660035
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-08-01 17:32:14 +0900

    💄 add comment design

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -60,6 +60,36 @@ function Post({ post }: Props) {
           />
         </div>
       </article>
+      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+      <form className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10">
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 mt-2" />
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Thomas Smith"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="thomassmith@email.com"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Add your comment here!"
+            rows={8}
+          />
+        </label>
+      </form>
     </main>
   );
 }
